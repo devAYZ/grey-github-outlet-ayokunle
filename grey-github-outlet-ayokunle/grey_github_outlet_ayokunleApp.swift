ce3bf8a0267ea8644d17f691caa413494bab0cfe
devAYZ <fatokimiemman@gmail.com>
2023-02-20 11:44:03 +0100

    add project file

---
+++ grey-github-outlet-ayokunle/grey_github_outlet_ayokunleApp.swift
@@ -0,0 +1,17 @@
+//
+//  grey_github_outlet_ayokunleApp.swift
+//  grey-github-outlet-ayokunle
+//
+//  Created by Ayokunle Pro on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct grey_github_outlet_ayokunleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ grey-github-outlet-ayokunleUITests/grey_github_outlet_ayokunleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  grey_github_outlet_ayokunleUITestsLaunchTests.swift
+//  grey-github-outlet-ayokunleUITests
+//
+//  Created by Ayokunle Pro on 2/20/23.
+//
+
+import XCTest
+
+final class grey_github_outlet_ayokunleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
